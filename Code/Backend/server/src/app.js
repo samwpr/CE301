548a68be33097a5e9a9484ecef67dd53e57ecf71
samwpr <swpr10x@gmail.com>
2022-12-30 11:40:59 +0800

    Setting up api

--- Code/Backend/server/src/app.js
+++ Code/Backend/server/src/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dbConnect = require('./config/dbConnect');
+const { registerUser } = require('./controllers/users/usersCtrl');
+const userRoute = require('./routes/users/usersRoute');
 const app = express();
 
 //dbConnect
 dbConnect();
 
+//routes
+app.use('/', userRoute);
+
+
 module.exports = app;
+
+
 
 
 
---
+++ Code/Backend/server/src/controllers/users/usersCtrl.js
@@ -0,0 +1,6 @@
+//Register a new user
+const registerUser = (req, res)=>{
+    res.json({user: "admin"});
+}
+
+module.exports = {registerUser}
